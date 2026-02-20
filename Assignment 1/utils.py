fb5541082edb06421505463158cac79d7ffd470c
Quokka <quokkaaa@QuokkadeMacBook-Pro.local>
2026-02-20 22:03:30 +0100

    K_finished

--- Assignment 1/utils.py
+++ Assignment 1/utils.py
@@ -88,7 +88,7 @@ def gauss_seidel(nx, ny, max_iter = 10000, tol = 1e-5):
     
     return u, errors
 
-def successive_over_relaxation(nx, ny, w, max_iter = 10000, tol = 1e-5, omega = 1.5):
+def successive_over_relaxation(nx, ny, w, max_iter = 10000, tol = 1e-5):
     '''Successive Over-Relaxation method for laplace equation'''
     u = np.zeros((nx, ny))
     u_new = np.zeros_like(u)
@@ -122,7 +122,7 @@ def successive_over_relaxation(nx, ny, w, max_iter = 10000, tol = 1e-5, omega = 
     
     return u, errors
 
-def successive_over_relaxation_it(nx, ny, w, max_iter = 10000, tol = 1e-5, omega = 1.5):
+def successive_over_relaxation_it(nx, ny, w, max_iter = 10000, tol = 1e-5):
     '''Successive Over-Relaxation method for laplace equation'''
     u = np.zeros((nx, ny))
     u_new = np.zeros_like(u)
@@ -150,6 +150,108 @@ def successive_over_relaxation_it(nx, ny, w, max_iter = 10000, tol = 1e-5, omega
         if np.max(np.abs(u_new - u)) < tol:
             return it
             
+        
+        # Update the solution for the next iteration
+        u[:] = u_new[:]
+    
+    return max_iter
+
+
+
+def successive_over_relaxation_sink(nx, ny, w, max_iter = 10000, tol = 1e-5,sinks=[[25,75,25,75,True]]):
+    '''Successive Over-Relaxation method for laplace equation'''
+    u = np.zeros((nx, ny))
+    u_new = np.zeros_like(u)
+    
+    # Set boundary conditions 
+    u[0, :] = 1  # Top boundary
+    u[-1, :] = 0  # Bottom boundary
+    u_new[0, :] = 1  # Top boundary
+    u_new[-1, :] = 0  # Bottom boundary
+    
+    # Set sink conditions
+    sink_exist = sinks is not None
+    def is_sink(i,j):
+        if not sink_exist:
+            return False
+        for sink in sinks:
+            x1,x2,y1,y2,positive = sink
+            if x1 <= j < x2 and y1 <= i < y2:
+                if positive:
+                    u_new[i,j] = 1
+                else:                   
+                    u_new[i,j] = 0
+                return True
+        return False
+    
+    errors = []
+    # Iteratively update the solution
+    for it in range(max_iter):
+        # Update each interior point
+        for i in range(1, ny - 1):
+            for j in range(nx):
+                if not is_sink(i,j):    
+                    jr = (j + 1) % (nx - 1)  # right boundary
+                    jl = (j - 1) % (nx - 1)  # left boundary
+                    u_new[i, j] = 0.25 * w * (u[i+1, j] + u_new[i-1, j] + u[i, jr] + u_new[i, jl]) + (1 -w) * u[i, j]
+        u_new[0, :] = 1  # Top boundary
+        u_new[-1, :] = 0  # Bottom boundary
+
+        errors.append(np.max(np.abs(u_new - u)))
+        # Check for convergence
+        if np.max(np.abs(u_new - u)) < tol:
+            print(f'Converged after {it} iterations.')
+            break
+        
+        # Update the solution for the next iteration
+        u[:] = u_new[:]
+    
+    return u, errors
+
+
+def successive_over_relaxation_sink_it(nx, ny, w, max_iter = 10000, tol = 1e-5,sinks=[[25,75,25,75,True]]):
+    '''Successive Over-Relaxation method for laplace equation'''
+    u = np.zeros((nx, ny))
+    u_new = np.zeros_like(u)
+    
+    # Set boundary conditions 
+    u[0, :] = 1  # Top boundary
+    u[-1, :] = 0  # Bottom boundary
+    u_new[0, :] = 1  # Top boundary
+    u_new[-1, :] = 0  # Bottom boundary
+    
+    # Set sink conditions
+    sink_exist = sinks is not None
+    def is_sink(i,j):
+        if not sink_exist:
+            return False
+        for sink in sinks:
+            x1,x2,y1,y2,positive = sink
+            if x1 <= j < x2 and y1 <= i < y2:
+                if positive:
+                    u_new[i,j] = 1
+                else:                   
+                    u_new[i,j] = 0
+                return True
+        return False
+    
+    errors = []
+    # Iteratively update the solution
+    for it in range(max_iter):
+        # Update each interior point
+        for i in range(1, ny - 1):
+            for j in range(nx):
+                if not is_sink(i,j):    
+                    jr = (j + 1) % (nx - 1)  # right boundary
+                    jl = (j - 1) % (nx - 1)  # left boundary
+                    u_new[i, j] = 0.25 * w * (u[i+1, j] + u_new[i-1, j] + u[i, jr] + u_new[i, jl]) + (1 -w) * u[i, j]
+        u_new[0, :] = 1  # Top boundary
+        u_new[-1, :] = 0  # Bottom boundary
+
+        errors.append(np.max(np.abs(u_new - u)))
+        # Check for convergence
+        if np.max(np.abs(u_new - u)) < tol:
+            return it
         
         # Update the solution for the next iteration
         u[:] = u_new[:]
