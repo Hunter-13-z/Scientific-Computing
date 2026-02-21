3363416140a6ea0fe6b3f060d586cf7a69622e37
Quokka <quokkaaa@QuokkadeMacBook-Pro.local>
2026-02-21 22:13:32 +0100

    insulator

--- Assignment 1/utils.py
+++ Assignment 1/utils.py
@@ -257,3 +257,64 @@ def successive_over_relaxation_sink_it(nx, ny, w, max_iter = 10000, tol = 1e-5,s
         u[:] = u_new[:]
     
     return max_iter
+
+
+
+def successive_over_relaxation_insulating(nx, ny, w, max_iter = 10000, tol = 1e-5,sinks=[[25,75,25,75]]):
+    '''Successive Over-Relaxation method for laplace equation'''
+    u = np.zeros((nx, ny))
+    u_new = np.zeros_like(u)
+    
+    # Set boundary conditions 
+    u[0, :] = 1  # Top boundary
+    u[-1, :] = 0  # Bottom boundary
+    u_new[0, :] = 1  # Top boundary
+    u_new[-1, :] = 0  # Bottom boundary
+    
+    # Set sink conditions
+    sink_exist = sinks is not None
+    def is_sink(i,j):
+        if not sink_exist:
+            return False
+        for sink in sinks:
+            x1,x2,y1,y2 = sink
+            if x1 <= j < x2 and y1 <= i < y2:
+                return True
+        return False
+    
+    errors = []
+
+    # Iteratively update the solution
+    for it in range(max_iter):
+        # Update each interior point
+        for i in range(1, ny - 1):
+            for j in range(nx):
+                if is_sink(i,j):    
+                     continue  
+                 
+                jr = (j + 1) % (nx - 1)  # right boundary
+                jl = (j - 1) % (nx - 1)  # left boundary
+
+                def get_value(i1,j1):
+                    if is_sink(i1,j1):
+                        return u_new[i,j]
+                    else:
+                        return u_new[i1,j1]
+                a = get_value(i+1,j)
+                b = get_value(i-1,j)
+                c = get_value(i,jr)
+                d = get_value(i,jl)
+                u_new[i, j] = 0.25 * w * (a + b + c + d) + (1 -w) * u[i, j]
+        u_new[0, :] = 1  # Top boundary
+        u_new[-1, :] = 0  # Bottom boundary
+
+        errors.append(np.max(np.abs(u_new - u)))
+        # Check for convergence
+        if np.max(np.abs(u_new - u)) < tol:
+            print(f'Converged after {it} iterations.')
+            break
+        
+        # Update the solution for the next iteration
+        u[:] = u_new[:]
+    
+    return u, errors
